394fb4543f77e4984b919fa8a99e484e95217bc7
Mirko Wagner <seelenvirtuose@gmail.com>
2021-05-16 17:50:47 +0200

    Initial setup.

--- .github/workflows/scripts/perform-maven-release.sh
+++ .github/workflows/scripts/perform-maven-release.sh
@@ -4,7 +4,7 @@ set -o errexit
 source .github/workflows/scripts/check-maven-distribution-settings.sh
 
 echo "::group::Performing the release with Maven"
-mvn ${MAVEN_DEFAULT_ARGS} build-helper:parse-version release:prepare release:perform \
+mvn ${MAVEN_DEFAULT_ARGS} ${MAVEN_ADDITIONAL_ARGS} build-helper:parse-version release:prepare release:perform \
 	-Darguments="${MAVEN_DEFAULT_ARGS} ${MAVEN_ADDITIONAL_ARGS}" \
 	-Dgoals="deploy" \
 	-DtagNameFormat="${TAG_NAME_FORMAT}" \
